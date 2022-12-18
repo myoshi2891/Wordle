b13d53e19efcc230b40c4dbafc36f1e9ad608c5c
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-12-18 18:28:06 +0900

    Build the Game Grid

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>TryCat</title>
+    <link rel="stylesheet" href="/css/app.css" />
+    <script src="/js/app.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+<body>
+    <div id="game">
+        <div class="row">
+            <div class="tile"></div>
+            <div class="tile"></div>
+            <div class="tile"></div>
+        </div>
+        <div class="row">
+            <div class="tile"></div>
+            <div class="tile"></div>
+            <div class="tile"></div>
+        </div>
+        <div class="row">
+            <div class="tile"></div>
+            <div class="tile"></div>
+            <div class="tile"></div>
+        </div>
+    </div>
+</body>
+</head>
+
+</html>
