449f6d77e37225214d6539605ab76b99c49b7cdd
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-12-18 21:15:08 +0900

    The Source of Truth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,5 @@
-let grid = document.querySelector('#game');
+import game from './game'
 
-let guessesAllowed = 4;
-let wordLength = 3;
-
-// let fragment = document.createDocumentFragment();
-
-// let generateGrid = () => {
-
-//     Array.from({length: guessesAllowed}).forEach(() => {
-//         let row = document.createElement('div');
-//         row.classList.add('row');
-
-//             Array.from({ length: wordLength }).forEach(() => {
-//                     let tile = document.createElement('div');
-//             tile.classList.add('tile');
-
-//                 row.appendChild(tile);
-//             });
-
-//         fragment.appendChild(row);
-//     });
-
-//     grid.appendChild(fragment);
-// }
-
-// generateGrid();
+document.addEventListener('alpine:init', () => {
+    Alpine.data('game', () => game);
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,18 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>TryCat</title>
-    {{-- <link rel="stylesheet" href="/css/app.css" /> --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://unpkg.com/alpinejs@3.10.5/dist/cdn.min.js"></script>
     {{-- <script src="/js/app.js" defer></script> --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    {{-- @vite(['resources/css/app.css']) --}}
 
 <body>
-    <div id="game" x-data="{ guessesAllowed: 4, wordLength: 3 }">
-        <template x-for="row in Array.from({ length: guessesAllowed })">
+    <div id="game" x-data="game" @keyup.window="onKeyPress($event.key)">
+        <template x-for="row in board">
             <div class="row">
-                <template x-for="tile in Array.from({ length: wordLength })">
-                    <div class="tile"></div>
+                <template x-for="tile in row">
+                    <div class="tile" x-text="tile.letter"></div>
                 </template>
             </div>
         </template>
