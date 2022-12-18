93a9f654ba72d1053687e292b304fb7b2a207973
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-12-19 00:55:02 +0900

    Submit a Guess

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,40 +1,57 @@
 import Tile from "./Tile";
 
-export default {
-            guessesAllowed: 3,
-            wordLength: 3,
-            currentRowIndex: 0,
+export default
+    {
+        guessesAllowed: 3,
+        theWord: 'cat',
+        wordLength: 3,
+        currentRowIndex: 0,
+      
+        get currentGuess() {
+            return this.currentRow.map(tile => tile.letter).join('');
+        },
 
-            init(){
-                this.board = Array.from({ length: this.guessesAllowed }, () => {
-                    return Array.from({ length: this.wordLength }, () => new Tile);
-                });
-            },
-            
-            onKeyPress(key) {
-                if (/^[A-z]$/.test(key)) {
-                    this.fillTile(key);
-                }
-            },
-
-            fillTile(key) {
-                for (let tile of this.currentRow()) {
-                    if (! tile.letter) {
-                        tile.fill(key);
-
-                        break;
-                    }
-                }
-
-                if (this.currentTileIndex === this.wordLength - 1) {
-                    this.currentRowIndex++;
-                    this.currentTileIndex = 0;
-                } else {
-                    this.currentTileIndex++;
-                }
-            },
-
-            currentRow() {
-                return this.board[this.currentRowIndex];
+        init(){
+            this.board = Array.from({ length: this.guessesAllowed }, () => {
+                return Array.from({ length: this.wordLength }, () => new Tile);
+            });
+        },
+        
+        onKeyPress(key) {
+            if (/^[A-z]$/.test(key)) {
+                this.fillTile(key);
+            } else if (key === 'Enter') {
+                this.submitGuess();
             }
+        },
+
+        fillTile(key) {
+            for (let tile of this.currentRow) {
+                if (! tile.letter) {
+                    tile.fill(key);
+
+                    break;
+                }
+            }
+        },
+                
+        submitGuess() {
+            let guess = this.currentGuess;
+
+            if (guess.length < this.wordLength) {
+                return;
+            }     
+            
+            if (guess === this.theWord) {
+                alert('You win!');
+            } else {
+                alert('Nope');
+
+                this.currentRowIndex++;
+            }
+        },
+
+        get currentRow() {
+                return this.board[this.currentRowIndex];
         }
+    }
